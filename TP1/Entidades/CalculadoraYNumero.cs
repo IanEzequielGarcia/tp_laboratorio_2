adccf94c7af8088531967e77004fde5bfa476e4e
Juan perez <garciaianavellaneda@gmail.com>
2021-04-15 19:04:31 -0300

    Documentaciones agregadas

---
+++ TP1/Entidades/CalculadoraYNumero.cs
@@ -0,0 +1,170 @@
+﻿using System;
+
+namespace Entidades
+{
+    public static class Calculadora
+    {
+        /// <summary>
+        /// Toma el string operador que contiene el calculo aritmetico deseado, y lo lleva a cabo con los objetos Numero1 y Numero2
+        /// </summary>
+        /// <param name="n1">el primer numero</param>
+        /// <param name="n2">el segundo numero</param>
+        /// <param name="operador">el operador ingresado</param>
+        /// <returns>el resultado de la operacion aritmetica</returns>
+        public static double Operador(Numero n1,Numero n2,string operador)
+        {
+            
+            if (!String.IsNullOrWhiteSpace(operador))
+                operador=ValidarOperador(operador.Trim()[0]);
+            switch(operador)
+            {
+                case "/":
+                    return n1 / n2;
+                case "*":
+                    return n1 * n2;
+                case "-":
+                    return n1 - n2;
+                default:
+                    return n1 + n2;
+            }
+        }
+        /// <summary>
+        /// Valida que el operador ingresado sea correcto (+ - * /), y de no ser así, devuelve +
+        /// </summary>
+        /// <param name="operador">el operador ingresado</param>
+        /// <returns>devuelve el operador ingresado, y de default + </returns>
+        static private string ValidarOperador(char operador)
+        {
+            if (operador == '/' || operador == '*' || operador == '-')
+                return $"{operador}";
+            else
+                return "+";
+        }
+    }
+    public class Numero
+    {
+        private double number;
+        /// <summary>
+        /// constructor por defecto
+        /// </summary>
+        public Numero()
+        {
+            this.number = 0;
+        }
+        /// <summary>
+        /// constructor con numero, que llama al constructor por string
+        /// </summary>
+        /// <param name="ingresado"></param>
+        public Numero(double ingresado):this(string.Format("{0}", ingresado))
+        {
+
+        }
+        /// <summary>
+        /// constructor por string, llama a la propiedad SetNumero
+        /// </summary>
+        /// <param name="ingresado"></param>
+        public Numero(string ingresado)
+        {
+            SetNumero = ingresado;
+        }
+        /// <summary>
+        /// Valida que el string ingresado sea un numero valido
+        /// </summary>
+        /// <param name="ingresado">el string ingresado con el numero</param>
+        /// <returns>el numero ya parseado de string a double</returns>
+        private double ValidarNumero(string ingresado)
+        {
+            double aux;
+            if (!double.TryParse(ingresado, out aux))
+                return 0;
+            return aux;
+        }
+        /// <summary>
+        /// Valida que el string ingresado sea un numero valido y lo setea
+        /// </summary>
+        private string SetNumero
+        {
+            set { this.number = ValidarNumero(value); }
+        }
+        /// <summary>
+        /// comprueba que el string ingresado sea binario
+        /// </summary>
+        /// <param name="elnumero">string ingresado</param>
+        /// <returns>devuelve true si es un binario valido o false si no lo es</returns>
+        bool EsBinario(string elnumero)
+        {
+            for(int i=0;i<elnumero.Length;i++)
+            {
+                if (String.IsNullOrWhiteSpace(elnumero) || elnumero[i] != '1'&& elnumero[i] != '0'||elnumero[0]=='-')
+                    return false;  
+            }
+            return true;
+        }
+        /// <summary>
+        /// convierte un numero double decimal en binario
+        /// </summary>
+        /// <param name="elNumero">el double a convertir en binario</param>
+        /// <returns>si es valido devuelve el numero convertido a binario y si no lo es devuelve "Valor invalido"</returns>
+        public string DecimalBinario(double elNumero)
+        {
+            Int64 aux2 = Convert.ToInt64(elNumero);
+            if (aux2 >= 0)
+                return Convert.ToString(aux2, 2);
+            return "Valor inválido";
+        }
+        /// <summary>
+        /// Convierte un numero binario guardado en un string a decimal
+        /// </summary>
+        /// <param name="elNumero">El numero a convertir</param>
+        /// <returns>si es valido devuelve el numero convertido a decimal y si no lo es devuelve "Valor invalido"</returns>
+        public string BinarioDecimal(string elNumero)
+        {
+            if (EsBinario(elNumero.ToString()))
+                return Convert.ToString(Convert.ToInt64(elNumero, 2));
+            return "Valor inválido";
+        }
+        /// <summary>
+        /// resta los numeros guardados en n1 y n2
+        /// </summary>
+        /// <param name="n1">el numero a restar</param>
+        /// <param name="n2">el numero que resta</param>
+        /// <returns>el resultado de la resta</returns>
+        public static double operator -(Numero n1, Numero n2)
+        {
+            return n1.number - n2.number;
+        }
+        /// <summary>
+        /// suma  los numeros guardados en n1 y n2
+        /// </summary>
+        /// <param name="n1">el primer numero a sumar</param>
+        /// <param name="n2">el segundo numero a sumar</param>
+        /// <returns>el resultado de la suma</returns>
+        public static double operator +(Numero n1, Numero n2)
+        {
+            return n1.number + n2.number;
+        }
+        /// <summary>
+        /// multiplica los numeros guardados en n1 y n2
+        /// </summary>
+        /// <param name="n1">el primer numero a multiplicar</param>
+        /// <param name="n2">el segundo numero a multiplicar</param>
+        /// <returns>el resultado de la multiplicacion</returns>
+        public static double operator *(Numero n1, Numero n2)
+        {
+            return n1.number * n2.number;
+        }
+        /// <summary>
+        /// divide los numeros guardados en n1 y n2
+        /// </summary>
+        /// <param name="n1">el dividendo</param>
+        /// <param name="n2">el divisor</param>
+        /// <returns>el resultado de la division,si se intenta dividir por 0 devuelte double.MinValue</returns>
+        public static double operator /(Numero n1, Numero n2)
+        {
+            if (n1.number != 0 && n2.number != 0)
+                return n1.number / n2.number;
+            else
+                return double.MinValue;
+        }
+    }
+}
---
+++ TP1/MiCalculadora/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiCalculadora
+{
+    public partial class FormCalculadora : Form
+    {
+        public FormCalculadora()
+        {
+            InitializeComponent();
+        }
+        static double Operar(Entidades.Numero elN1, Entidades.Numero elN2, string elOperador)
+        {
+            return Entidades.Calculadora.Operador(elN1, elN2, elOperador);
+        }
+        /// <summary>
+        /// realiza la operacion aritmetica entre los dos numeros a traves del metodo Operar
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnOperar_Click(object sender, EventArgs e)
+        {
+            Entidades.Numero numeroUno = new Entidades.Numero(this.txtNumero1.Text);
+            Entidades.Numero numeroDos = new Entidades.Numero(this.txtNumero2.Text);
+            this.labelResultado.Text = (FormCalculadora.Operar(numeroUno, numeroDos, this.comboBox2.Text)).ToString();
+        }
+
+        /// <summary>
+        /// Llama a la funcion Limpiar
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnLimpiar_Click(object sender, EventArgs e)
+        {
+            this.Limpiar();
+        }
+        /// <summary>
+        /// "Limpia" Los campos de texto del form
+        /// </summary>
+        private void Limpiar()
+        {
+            this.labelResultado.Text = "";
+            this.txtNumero1.Text = "";
+            this.txtNumero2.Text = "";
+            this.comboBox2.Text = "";
+        }
+        /// <summary>
+        /// Cierra el Windows form
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        /// <summary>
+        /// convierte el resultado de la operacion aritmetica a binario
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnDecABin_Click(object sender, EventArgs e)
+        {
+            string num = this.labelResultado.Text;
+            Entidades.Numero aux = new Entidades.Numero(num);
+            if (String.IsNullOrWhiteSpace(num))
+                this.labelResultado.Text = "Valor inválido";
+            else
+            this.labelResultado.Text=aux.DecimalBinario(Double.Parse(num));
+        }
+        /// <summary>
+        /// convierte el resultado de la operacion aritmetica de binario a decimal
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnBinADec_Click(object sender, EventArgs e)
+        {
+            string num = this.labelResultado.Text;
+            Entidades.Numero aux = new Entidades.Numero(num);
+            if (String.IsNullOrWhiteSpace(num))
+                this.labelResultado.Text = "Valor inválido";
+            else
+                this.labelResultado.Text = aux.BinarioDecimal(num);
+        }
+    }
+}
