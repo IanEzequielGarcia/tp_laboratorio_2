74bf33437d6a3d0d3cb6328b45f7c257d8497967
Juan perez <garciaianavellaneda@gmail.com>
2021-05-05 12:53:39 -0300

    Terminado

---
+++ TP2/TP-02/Entidades/Sedan.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Entidades
+{
+    public class Sedan : Vehiculo
+    {
+        public enum ETipo { CuatroPuertas, CincoPuertas }
+        ETipo tipo;
+
+        /// <summary>
+        /// Por defecto, TIPO será CuatroPuertas
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Sedan(EMarca marca, string chasis, ConsoleColor color)
+            : base(chasis, marca, color)
+        {
+            tipo = ETipo.CuatroPuertas;
+        }
+
+        /// <summary>
+        /// Sedan son 'Mediano'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Mediano;
+            }
+        }
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("SEDAN");
+            sb.AppendFormat("{0}", base.Mostrar());
+            sb.AppendLine("");
+            sb.AppendFormat("TAMAÑO : {0}", this.Tamanio);
+            sb.AppendLine("TIPO : " + this.tipo);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
